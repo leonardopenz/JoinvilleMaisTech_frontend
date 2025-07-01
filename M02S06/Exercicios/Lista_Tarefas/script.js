215011e8015d3264e970b6c6067c1ed099eeac09
Leonardo Penz <leonardopenz.ti@gmail.com>
2025-07-01 11:18:11 -0300

    Card 02

--- M02S06/Exercicios/Lista_Tarefas/script.js
+++ M02S06/Exercicios/Lista_Tarefas/script.js
@@ -1,20 +1,20 @@
 let tarefa = document.getElementById("tarefa");
 
-document.addEventListener("DOMContentLoaded", carregarTarefas);
+document.addEventListener("DOMContentLoaded", () => {
+  carregarTarefas();
+  carregarConcluidas();
+});
 
 function add() {
-  const lista = document.getElementById("listagem");
+  //const lista = document.getElementById("listagem");
 
   if (tarefa.value.trim() === "") return;
 
-  let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
-
+  let tarefas = JSON.parse(localStorage.getItem("tarefasPendentes")) || [];
   tarefas.push(tarefa.value.trim());
-
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  localStorage.setItem("tarefasPendentes", JSON.stringify(tarefas));
 
   carregarTarefas();
-
   limparCampo();
 }
 
@@ -22,21 +22,80 @@ function carregarTarefas() {
   const lista = document.getElementById("listagem");
   lista.innerHTML = "";
 
-  const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+  const tarefas = JSON.parse(localStorage.getItem("tarefasPendentes")) || [];
 
   tarefas.forEach((t, index) => {
     const li = document.createElement("li");
-    li.textContent = t;
+
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.checked = false;
+    check.onchange = () => concluirTarefa(index);
+
+    const texto = document.createElement("span");
+    texto.textContent = t;
 
     const btn = document.createElement("button");
     btn.textContent = "Excluir";
     btn.classList.add("btn-excluir");
     btn.onclick = () => excluirTarefa(index);
 
+    li.appendChild(check);
+    li.appendChild(texto);
     li.appendChild(btn);
     lista.appendChild(li);
+  });
+}
+
+function carregarConcluidas() {
+  const lista = document.getElementById("concluidas");
+  lista.innerHTML = "";
+
+  const concluidas = JSON.parse(localStorage.getItem("tarefasConcluidas")) || [];
+
+  concluidas.forEach((t, index) => {
+    const li = document.createElement("li");
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = true;
+    checkbox.onchange = () => desmarcarTarefa(index);
+
+    const texto = document.createElement("span");
+    texto.textContent = t;
+
+    li.appendChild(checkbox);
+    li.appendChild(texto);
     lista.appendChild(li);
   });
+}
+
+function concluirTarefa(index) {
+  let pendentes = JSON.parse(localStorage.getItem("tarefasPendentes")) || [];
+  let concluidas = JSON.parse(localStorage.getItem("tarefasConcluidas")) || [];
+
+  const tarefaConcluida = pendentes.splice(index, 1)[0];
+  concluidas.push(tarefaConcluida);
+
+  localStorage.setItem("tarefasPendentes", JSON.stringify(pendentes));
+  localStorage.setItem("tarefasConcluidas", JSON.stringify(concluidas));
+
+  carregarTarefas();
+  carregarConcluidas();
+}
+
+function desmarcarTarefa(index) {
+  let pendentes = JSON.parse(localStorage.getItem("tarefasPendentes")) || [];
+  let concluidas = JSON.parse(localStorage.getItem("tarefasConcluidas")) || [];
+
+  const tarefaVoltando = concluidas.splice(index, 1)[0];
+  pendentes.push(tarefaVoltando);
+
+  localStorage.setItem("tarefasPendentes", JSON.stringify(pendentes));
+  localStorage.setItem("tarefasConcluidas", JSON.stringify(concluidas));
+
+  carregarTarefas();
+  carregarConcluidas();
 }
 
 function excluirTarefa(index) {
